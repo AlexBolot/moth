dc47d4342a045b1cf86d40d03c74d67130b5cc12
Jason Haenlin <jasonjas5252@gmail.com>
2018-12-07 03:36:20 +0100

    ajoute une sidebar et des cards

---
+++ angular/moth/src/app/components/card-time-left/card-time-left.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-time-left',
+  templateUrl: './card-time-left.component.html',
+  styleUrls: ['./card-time-left.component.css']
+})
+export class CardTimeLeftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular/moth/src/app/components/dashboard/dashboard.component.ts
+++ angular/moth/src/app/components/dashboard/dashboard.component.ts
@@ -13,21 +13,19 @@ export class DashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Wheather', cols: 1, rows: 1 },
+          { title: 'Graph', cols: 1, rows: 1 },
+          { title: 'Health Care', cols: 1, rows: 1 }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Weather', cols: 1, rows: 1 },
+        { title: 'Graph', cols: 1, rows: 2 },
+        { title: 'Health Care', cols: 1, rows: 1 }
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 }
